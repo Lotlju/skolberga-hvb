4e6bb39d5d54f1a4e417e8426d07740011f5ecff
Lotlju <lotta.stenhardt@gmail.com>
2026-02-09 14:32:07 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 // Konfiguration för Skolberga HVB App
 const CONFIG = {
     // Google Apps Script Web App URL
-    // DU MÅSTE UPPDATERA DENNA efter du skapat scriptet!
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxGfNoTXVVPMKgLHzEL2BX5kUSeIEgZqj1GPf31wTmZtLtPCE73SS6xYKudxSproUvzNg/exec
-',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxGfNoTXVVPMKgLHzEL2BX5kU5eIEgZqj1GPf31wTmZtLtPCE73SS6xYKudXSproUvzNg/exec',
     
     // Google Sheet ID (för referens)
     SHEET_ID: '1cfKjlxUOpdXYIYEzwEsPAsmV81tLeiZ7FHgWi38uU6Q'
